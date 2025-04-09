4a59d90d1ea7fe4acb0546f0eaf388d75e179e02
nathan-frost <164823855+nathan-frost@users.noreply.github.com>
2025-04-09 03:19:55 +0000

    updated photo show

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,10 @@ Rails.application.routes.draw do
   resources :follow_requests
   resources :comments
   resources :photos
-  get "/:username" => "users#show", as: :user
+  resources :users, only: [ :index ]
+
+  get ":username" => "users#show", as: :user
+  get ":username/liked" => "users#liked", as: :liked
   # resources :users, only: :show
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
