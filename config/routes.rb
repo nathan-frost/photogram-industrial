2134c4987d50cc1f9057263dd240d93dff63f7fb
nathan-frost <164823855+nathan-frost@users.noreply.github.com>
2025-04-09 03:26:06 +0000

    starting discover page

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,10 @@ Rails.application.routes.draw do
 
   get ":username" => "users#show", as: :user
   get ":username/liked" => "users#liked", as: :liked
+
+  
+
+
   # resources :users, only: :show
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
