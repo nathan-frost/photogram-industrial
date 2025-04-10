a28edea43af98908012cc27c80878c4adc81a5ec
nathan-frost <164823855+nathan-frost@users.noreply.github.com>
2025-04-10 00:59:05 +0000

    Updated photo creation

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -24,7 +24,7 @@ class PhotosController < ApplicationController
   # POST /photos or /photos.json
   def create
     @photo = Photo.new(photo_params)
-
+    @photo.owner = current_user
     respond_to do |format|
       if @photo.save
         format.html { redirect_to @photo, notice: "Photo was successfully created." }
